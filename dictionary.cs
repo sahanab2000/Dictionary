2aae1113186119f0065da7f60e95818887d50085
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-11-13 12:52:24 +0530

    Add files via upload

---
+++ dictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Collections");
+            doDictionaryDemo();
+        }
+        public static void doDictionaryDemo()
+        {
+            Console.WriteLine("\nIn doDictionaryDemo");
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(100, "Ishani");
+            dictionary.Add(101, "krithi");
+            dictionary.Add(102, "Aman");
+
+            Console.WriteLine("Access value using key(key=100): " + dictionary [102]);
+
+            Console.WriteLine("\nIterating Dictionary:");
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine("Key =" + element.Key + " & Value =" +element.Value);
+            }
+        }
+    }
+}
